248b29e689d8500090d418a7048c7eb3fd00ad40
Sreejish <102538251+sree-jish@users.noreply.github.com>
2023-12-07 17:43:52 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,51 +1,37 @@
-// Create a web server
-// GET /comments - returns all comments
-// POST /comments - add a new comment
-// DELETE /comments/:id - delete a comment
-// PUT /comments/:id - update a comment
+// Create web server which will allow users to add comments to a file
+// and view all comments for a file.
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+// Load the http module to create an http server.
+var http = require('http');
+var fs = require('fs');
 
-app.use(bodyParser.json());
-
-const comments = [
-    {id: 1, body: 'first comment'},
-    {id: 2, body: 'second comment'},
-    {id: 3, body: 'third comment'}
-];
-
-// GET /comments - returns all comments
-app.get('/comments', (req, res) => {
-    res.json(comments);
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+    if (request.method === 'GET' && request.url === '/comments') {
+        fs.readFile('comments.txt', function(err, data) {
+            if (err) throw err;
+            response.writeHead(200, {"Content-Type": "text/plain"});
+            response.write(data);
+            response.end();
+        });
+    } else if (request.method === 'POST' && request.url === '/comments') {
+        var body = '';
+        request.on('data', function (data) {
+            body += data;
+        });
+        request.on('end', function () {
+            fs.appendFile('comments.txt', body + "\n", function(err) {
+                if (err) throw err;
+                response.writeHead(200, {"Content-Type": "text/plain"});
+                response.write("Comment added: " + body + "\n");
+                response.end();
+            });
+        });
+    } else {
+        response.writeHead(404, {"Content-Type": "text/plain"});
+        response.write("404 Not Found\n");
+        response.end();
+    }
 });
 
-// POST /comments - add a new comment
-app.post('/comments', (req, res) => {
-    const comment = req.body;
-    comment.id = comments.length + 1;
-    comments.push(comment);
-    res.json(comment);
-});
-
-// DELETE /comments/:id - delete a comment
-app.delete('/comments/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const index = comments.findIndex(comment => comment.id === id);
-    comments.splice(index, 1);
-    res.json({deleted: true});
-});
-
-// PUT /comments/:id - update a comment
-app.put('/comments/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const comment = req.body;
-    const index = comments.findIndex(comment => comment.id === id);
-    comments[index] = comment;
-    res.json(comment);
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+// Listen on port 8000, IP defaults to
